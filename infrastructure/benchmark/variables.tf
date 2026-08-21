c156e8429525d403f00d33747d65c084a58e4493
mahaveerreddysamala <mahaveerreddysamala21@gmail.com>
2026-08-21 16:49:37 -0500

    fix: format Terraform variable blocks

--- infrastructure/benchmark/variables.tf
+++ infrastructure/benchmark/variables.tf
@@ -1,6 +1,29 @@
-variable "aws_region" { type = string default = "us-east-1" }
-variable "bucket_name" { type = string description = "Globally unique S3 bucket name" }
-variable "vpc_id" { type = string description = "Existing VPC ID" }
-variable "subnet_id" { type = string description = "Private subnet ID with NAT egress" }
-variable "instance_type" { type = string default = "m6i.2xlarge" }
-variable "root_volume_gb" { type = number default = 150 }
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "bucket_name" {
+  type        = string
+  description = "Globally unique S3 bucket name"
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "Existing VPC ID"
+}
+
+variable "subnet_id" {
+  type        = string
+  description = "Private subnet ID with NAT egress"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "m6i.2xlarge"
+}
+
+variable "root_volume_gb" {
+  type    = number
+  default = 150
+}
